903208bead1c273d0f59c39b860f67e08304e92d
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-03-26 16:28:24 -0300

    Update
    Para formalizar a entrega do componente de extração, foi criado uma classe e um método com todo o código. Assim, quando for preciso extrair, basta instanciar a classe e invocar o método

--- extrair_dados.py
+++ extrair_dados.py
@@ -1,32 +1,42 @@
-import requests 
+import requests
+from datetime import datetime 
 
-dados = requests.get('https://servicodados.ibge.gov.br/api/v2/cnae/classes').json()
+class ETL:
+    def __init__(self):
+        self.url = None
 
-# Onde os dados extraídos estão guardados
-dados[0]
+    def extract_cnae_data(self, url):
+        self.url = url
+        data_extracao = datetime.today().strftime("%Y/%m/%d - %H:%M:%S")
+        # Faz a extração
+        dados = requests.get(self.url).json()
 
-# Lista de todos os grupos
-qtde_atividades_distintas = len(dados)
+        # Extrai os grupos de registro
+        grupos = []
+        for registro in dados:
+            grupos.append(registro['grupo']['descricao'])
 
-grupos = []
-for registro in dados:
-    grupos.append(registro['grupo']['descricao'])
+        # Cria uma lista de tuplas (grupo, quantidade_atividades)
+        grupos_count = [(grupo, grupos.count(grupo)) for grupo in set(grupos)]
+        grupos_count = dict(grupos_count)
 
-grupos[:10]
+        # Capturar o valor máximo de atividades
+        valor_maximo = max(grupos_count.values())
+        # Gera uma lista com os grupos que possuem a quantidade máxima de atividades
+        grupos_mais_atividades = [chave for (chave, valor) in grupos_count.items() if valor == valor_maximo]
 
-# Axtrair a quantidade de grupos de atividades
-qtde_grupos_distintos = len(set(grupos))
+        # Informações
+        qtde_atividades_distintas = len(dados)
+        qtde_grupos_distintos = len(set(grupos))
 
-# Contar quantas atividades estão cadastradas em cada grupo
-grupos_count = [(grupo, grupos.count(grupo)) for grupo in set(grupos)]
-grupos_count[:5]
+        print(f"\n{'-' * 74} Extrato {'-' * 74}")
+        print(f"Dados extraídos em: {data_extracao}")
+        print(f"Quantidade de atividades distintas = {qtde_atividades_distintas}")
+        print(f"Quantidade de grupos distintos = {qtde_grupos_distintos}")
+        print(f"Grupos com o maior número de atividades = {grupos_mais_atividades}, atividades = {valor_maximo}")
+        print('-' * 157)
+            
+        return None
 
-# Quais grupos possuem o maior número de atividades vinculadas
-grupos_count = dict(grupos_count)
-
-# Descobrir qual (ou quais) grupos possuem mais atividades
-valor_maximo = max(grupos_count.values())
-grupos_mais_atividades = [chave for (chave, valor) in grupos_count.items() if valor == valor_maximo]
-
-grupos_mais_atividades
-
+# Usando a classe ETL
+ETL().extract_cnae_data('https://servicodados.ibge.gov.br/api/v2/cnae/classes')
